fd89fca51669c1867e80be7fc2f54f6009407206
Flavio <flaviodervishi239@gmail.com>
2026-03-29 00:19:02 +0100

    Vendosja e testimit

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import "./index.css";
 import App from "./App.jsx";
-import "@fontsource/cormorant-garamond"
-import "@fontsource/inter"
-
+import "@fontsource/cormorant-garamond";
+import "@fontsource/inter";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <SpeedInsights />
     </BrowserRouter>
   </StrictMode>
 );
